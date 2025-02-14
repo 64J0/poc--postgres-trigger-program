90c907e5d403f172842ac3ed3e0b01436d71962a
64J0 <vinigaio97@gmail.com>
2025-02-14 18:44:09 -0300

    wip: create mailbox processor

--- manager/Program.fs
+++ manager/Program.fs
@@ -5,40 +5,41 @@ module Main =
     open Npgsql
     open FsToolkit.ErrorHandling
 
-    type Message = ExecuteProgram of ExecutionId: int
+    type Message =
+        | ExecuteProgram of ExecutionId: int
+        | HandleExecutionSuccess of unit
+        | HandleExecutionFailure of unit
 
-    type Manager = MailboxProcessor<Message>
-
-    let processor (manager: Manager) =
-        let rec loop () =
-            async {
-                let! message = manager.Receive()
-
-                match message with
-                | ExecuteProgram _executionId -> return! loop ()
-            }
-
-        loop
-
-    let start () =
-        MailboxProcessor.Start(fun manager -> processor manager ())
-
-    let executeProgram (manager: Manager) =
-        manager.Post(ExecuteProgram 1)
-        manager
+    let processor =
+        MailboxProcessor.Start(fun inbox ->
+            let rec loop () =
+                async {
+                    let! msg = inbox.Receive()
+                    match msg with
+                    | Message.ExecuteProgram executionId ->
+                        // 1. get the program details from database using the execution id
+                        // 2. run the program and collect the result
+                        // 3. call the next stage if the execution was a success or a failure
+                    | HandleExecutionSuccess ->
+                        // 1. write the output to the database
+                    | HandleExecutionFailure ->
+                        // 1. write the output to the database
+                        // 2. print to the error stream
+                })
 
     [<EntryPoint>]
     let main (_args: string[]) : int =
         result {
             let handler =
                 fun (_sender: obj) (eventArgs: NpgsqlNotificationEventArgs) ->
-                    printfn "Received notification"
+                    printfn "Notification received"
                     printfn "Event args payload: %A" eventArgs.Payload
 
             let notificationEventHandler = NotificationEventHandler(handler)
             let! conn = Shared.Database.Main.getAsyncConnection notificationEventHandler
 
             while true do
+                printfn "Started listening for Postgres notifications... "
                 conn.Wait()
 
             return 0
