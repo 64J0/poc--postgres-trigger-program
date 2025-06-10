a77d79486c0c9f8262da3edea28b5c99c5a9342a
64J0 <vinigaio97@gmail.com>
2025-06-10 18:10:52 -0300

    update wait sample to return a non-zero exit status code

--- samples/wait.fsx
+++ samples/wait.fsx
@@ -1,13 +1,29 @@
 // useful script to test the failure
 
-let wait (time: int) =
-    printfn "Waiting for %i ms" time
-    System.Threading.Thread.Sleep time
-    printfn "Wait finished"
+let SUCCESS_STATUS_CODE = 0
+let FAILURE_STATUS_CODE = 1
 
-let args = fsi.CommandLineArgs
+let main () : Async<int> =
+    async {
+        try
+            let args = fsi.CommandLineArgs
 
-if args.Length < 2 then
-    eprintfn "Please inform the time to wait!"
-else
-    wait (int args.[1])
+            if args.Length < 2 then
+                eprintfn "Please inform the time to wait!"
+                exit FAILURE_STATUS_CODE
+                return FAILURE_STATUS_CODE
+            else
+                let sleepTime = int args.[1]
+                printfn "Sleeping for %i ms" sleepTime
+                do! Async.Sleep(sleepTime)
+                printfn "Sleep finished"
+
+                exit SUCCESS_STATUS_CODE
+                return SUCCESS_STATUS_CODE
+        with exn ->
+            eprintfn "An exception was thrown: %A" exn
+            exit FAILURE_STATUS_CODE
+            return FAILURE_STATUS_CODE
+    }
+
+main () |> Async.RunSynchronously
